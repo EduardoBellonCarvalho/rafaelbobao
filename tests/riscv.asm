f74c5aa7c011868ca1cfb2e748a8743fbab5a7cb
Eduardo Carvalho <140848952+EduardoBellonCarvalho@users.noreply.github.com>
2025-10-29 11:54:57 -0300

    Update riscv.asm

--- tests/riscv.asm
+++ tests/riscv.asm
@@ -1,17 +1,22 @@
-.text	# 0x00000000 
-.globl _start
+.text 
+
+.global_start
+
 _start:
-	la s0, b
-	lw t0, -4(s0)
-	lw t1, (s0)
-loop:
-	add t2, t1, t0
-	add t0, zero, t1
-	add t1, zero, t2
-	addi s0, s0, 4
-	sw t2, (s0)
-	j loop
-	
-.data	# 0x00000100 
-a:	.word 0
-b:	.word 1
+
+lw s0, 0x00000100
+lw t0, 0x00000200    #ultimo led
+lw t1, 0x00000001    #primeiro led
+lw t2, 0x00000000    #comeco(fofo) uWu
+
+senhorloop
+
+slli t2,t2,1
+sw t2,4(s0)
+bne t0,t2,senhorloop
+
+corinthiansmenotti2012
+srli t2,t2,1
+sw t2,4(s0)
+bne t0,t1,corinthiansmenotti2012
+j senhorloop
