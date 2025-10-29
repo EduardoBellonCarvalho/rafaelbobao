0da0a003a750ecff2dd981dbf0225a90b33dca5e
Eduardo Carvalho <140848952+EduardoBellonCarvalho@users.noreply.github.com>
2025-10-29 11:58:08 -0300

    Update top.sv

--- top.sv
+++ top.sv
@@ -14,19 +14,18 @@ module top(
   assign reset = ~KEY[0]; // active low
     
   // microprocessor
-  riscvmono cpu(clk, reset, pc, instr, addr, writedata, memwrite, readdata);
+  riscvmulti cpu(clk, reset, addr, writedata, memwrite, readdata);
 
-  // instructions memory 
-  rom instr_mem(pc, instr);
-
-  // data memory 
-  ram data_mem(clk, memwrite & isRAM, addr, writedata, readdata);
+  // memory 
+  mem ram(clk, memwrite, addr, writedata, MEM_readdata);
 
   // memory-mapped i/o
   wire isIO  = addr[8]; // 0x0000_0100
   wire isRAM = !isIO;
   localparam IO_LEDS_bit = 2; // 0x0000_0104
   localparam IO_HEX_bit  = 3; // 0x0000_0108
+  localparam IO_KEY_bit  = 4; // 0x0000_0110 
+  localparam IO_SW_bit   = 5; // 0x0000_0120
   reg [23:0] hex_digits; // memory-mapped I/O register for HEX
   dec7seg hex0(hex_digits[ 3: 0], HEX0);
   dec7seg hex1(hex_digits[ 7: 4], HEX1);
@@ -35,10 +34,13 @@ module top(
   dec7seg hex4(hex_digits[19:16], HEX4);
   dec7seg hex5(hex_digits[23:20], HEX5);
   always @(posedge clk)
-    if (memwrite & isIO) begin // memory-mapped I/O
+    if (memwrite & isIO) begin // I/O write 
       if (addr[IO_LEDS_bit])
         LEDR <= writedata;
       if (addr[IO_HEX_bit])
         hex_digits <= writedata;
   end
-endmodule
+  assign IO_readdata = addr[IO_KEY_bit] ? {32'b0, KEY} :
+                       addr[ IO_SW_bit] ? {32'b0,  SW} : 
+                                           32'b0       ;
+  assign readdata = isIO ? IO_readdata : MEM_readdata; 
